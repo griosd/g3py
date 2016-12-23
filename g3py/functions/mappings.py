6e8c833506ee7ee699dcd8544e2e01a66ff6bc5d
grios <gon.rios.diaz@gmail.com>
2016-12-23 03:12:46 -0300

    Logistic Transform Fix #6

--- g3py/functions/mappings.py
+++ g3py/functions/mappings.py
@@ -1,9 +1,8 @@
 import numpy as np
-import theano as th
 import theano.tensor as tt
 import theano.sandbox.linalg as sT
 from g3py.functions.hypers import Hypers
-
+from g3py.libs.tensors import tt_to_num, debug
 
 class Mapping(Hypers):
     def __call__(self, x):
@@ -13,7 +12,10 @@ class Mapping(Hypers):
         pass
 
     def logdet_dinv(self, y):
-        return tt.log(sT.det(tt.jacobian(self.inv(y), y)))
+        pass
+
+    def automatic_logdet_dinv(self, y):
+        return debug(tt.log(sT.det(debug(tt.jacobian(self.inv(y), y), 'jacobian_inv'))), 'automatic_logdet_dinv')
 
 
 class Identity(Mapping):
@@ -81,3 +83,40 @@ class BoxCoxShifted(Mapping):
 
     def logdet_dinv(self, y):
         return (self.power - 1.0)*tt.sum(tt.log(tt.abs_(y+self.shift)))
+
+
+class Logistic(Mapping):
+    def __init__(self, y, name='Logistic', lower=None, high=None, location=None, scale=None):
+        super().__init__(y, name)
+        self.lower = lower
+        self.high = high
+        self.location = location
+        self.scale = scale
+
+    def check_hypers(self, parent=''):
+        if self.lower is None:
+            self.lower = Hypers.Flat(parent+self.name+'_lower', shape=self.shape)
+        if self.high is None:
+            self.high = Hypers.FlatExp(parent+self.name+'_high', shape=self.shape)
+        if self.location is None:
+            self.location = Hypers.Flat(parent+self.name+'_location', shape=self.shape)
+        if self.scale is None:
+            self.scale = Hypers.FlatExp(parent+self.name+'_scale', shape=self.shape)
+        self.hypers += [self.lower, self.high, self.location, self.scale]
+
+    def default_hypers(self, x=None, y=None):
+        return {self.lower: np.float32(1.5*np.min(y) - 0.5*np.max(y)),
+                self.high: np.float32(2.0*(np.max(y) - np.min(y))),
+                self.location: np.float32(np.mean(y)),
+                self.scale: np.float32(np.std(y))}
+
+    def __call__(self, x):
+        return self.lower + self.high*(0.5 + 0.5*tt.tanh((x - self.location)/(2*self.scale)))
+
+    def inv(self, y):
+        p = tt.switch(y < self.lower, 0, tt.switch(y > self.lower + self.high, 1, (y - self.lower) / self.high))
+        return debug(self.location + self.scale*tt_to_num(tt.log(p / (1 - p))), 'inv')
+
+    def logdet_dinv(self, y):
+        p = tt.switch(y < self.lower, 0, tt.switch(y > self.lower + self.high, 1, (y - self.lower) / self.high))
+        return debug(tt.sum(tt_to_num(tt.log(self.scale / (self.high * p * (1-p))))), 'logdet_dinv')
