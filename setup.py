31a08e74ab1d686953f759205c622f90b38ece6f
grios <gon.rios.diaz@gmail.com>
2017-03-09 01:38:01 -0300

    Imports

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='g3py',
-      version='0.2.4.9',
+      version='0.2.5.0',
       description='Generalized Graphical Gaussian Processes',
       url='https://github.com/griosd/g3py',
       author='Gonzalo Rios',
@@ -9,8 +9,11 @@ setup(name='g3py',
       license='MIT',
       packages=find_packages(),
       install_requires=[
-          'numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn', 'theano>=0.9', 'pymc3', 'emcee', 'scikit-learn', 'statsmodels'
+          'numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn', 'theano>=0.9', 'pymc3', 'emcee', 'scikit-learn>=0.18', 'statsmodels'
       ],
+      extras_require = {
+              'load_data':  ['statsmodels']
+      },
       tests_require=[
             'pytest', 'pytest-mpl'
       ],
