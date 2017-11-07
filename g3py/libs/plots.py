694020bc800acd36dfe54a9e62e397e8c643443b
Camilo Iturra <citurra@noisegrasp.com>
2017-11-07 13:03:36 -0300

    error cache de plot_img

--- g3py/libs/plots.py
+++ g3py/libs/plots.py
@@ -97,10 +97,12 @@ def plot_save(file='example.pdf'):
     os.makedirs(file[:file.rfind('/')], exist_ok=True)
     plt.savefig(file, bbox_inches='tight')
 
+
 def plot_img(name='example', path='plots/', extension='png'):
     file = path + name+'.'+extension
     os.makedirs(file[:file.rfind('/')], exist_ok=True)
     plt.savefig(file, bbox_inches='tight')
+    plt.close()
     display.display(display.HTML('<img src=\'{}?{}\'>'.format(file, np.random.rand())))
 
 
