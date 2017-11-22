56de0e440613781614a79ad92a172f42ee42c36c
camilo iturra <citurra@noisegrasp.com>
2017-11-22 15:02:27 -0300

    MKL_THREADING_LAYER

--- g3py/config.py
+++ g3py/config.py
@@ -1,9 +1,10 @@
+import os
 import theano as th
 
 th.config.floatX = 'float32'
 th.config.on_unused_input = 'ignore'
 th.config.mode = 'FAST_RUN'
-
+os.environ['MKL_THREADING_LAYER'] = 'GNU'
 #th.config.warn_float64 = 'warn'
 #th.config.cast_policy = 'numpy+floatX'
 #th.config.int_division = 'raise'
