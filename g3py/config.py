cd579d3da53325fa57570d48086e9c3cffd3b7ca
grios <gon.rios.diaz@gmail.com>
2018-05-28 16:15:37 -0400

    np.float32

--- g3py/config.py
+++ g3py/config.py
@@ -6,7 +6,7 @@ th.config.floatX = 'float32'
 th.config.on_unused_input = 'ignore'
 th.config.mode = 'FAST_RUN'
 
-#th.config.warn_float64 = 'warn'
+th.config.warn_float64 = 'raise'
 #th.config.cast_policy = 'numpy+floatX'
 #th.config.int_division = 'raise'
 
