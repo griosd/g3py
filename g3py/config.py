75333ac0b4566f8e5c6f36dd3f563c86290579d1
grios <gon.rios.diaz@gmail.com>
2019-03-04 14:14:45 -0300

    amdlibm = False

--- g3py/config.py
+++ g3py/config.py
@@ -5,9 +5,10 @@ import theano as th
 th.config.floatX = 'float32'
 th.config.on_unused_input = 'ignore'
 th.config.mode = 'FAST_RUN'
-
+th.config.lib.amdlibm = False
 th.config.warn_float64 = 'raise'
 #th.config.cast_policy = 'numpy+floatX'
 #th.config.int_division = 'raise'
+
 
 plot_big = False
